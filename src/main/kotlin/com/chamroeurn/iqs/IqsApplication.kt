0d86c544ea488e3e9c8460dedf4124128801520c
Duch Chamroeun <chamroeurn@soramitsukhmer.com>
2025-04-25 14:50:36 +0700

    initial project

---
+++ src/main/kotlin/com/chamroeurn/iqs/IqsApplication.kt
@@ -0,0 +1,11 @@
+package com.chamroeurn.iqs
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class IqsApplication
+
+fun main(args: Array<String>) {
+	runApplication<IqsApplication>(*args)
+}
